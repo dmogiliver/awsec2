8ad739014c1b24b37b243e26583b72aabe06ba97
Dmitri Mogiliver <dmogiliver@gmail.com>
2019-04-11 10:03:17 -0700

    Final version of working terraform code. Image name ami-005bdb005fb00e791

--- servers.tf
+++ servers.tf
@@ -2,8 +2,8 @@
 resource "aws_instance" "dmogiliver-ec2-instance" {
   ami = "${var.ami_id}"
   instance_type = "t2.micro"
-  key_name = "${var.ami_key_pair_name}"
   security_groups = ["${aws_security_group.ingress-all-dmogiliver.id}"]
+  key_name = "dmogiliver-IAM-keypair2"
 
 tags {
     Name = "${var.ami_name}"
--- variables.tf
+++ variables.tf
@@ -4,4 +4,4 @@ variable "ami_name" {}
 
 variable "ami_id" {}
 
-variable "ami_key_pair_name" {}
+#variable "ami_key_pair_name" {}
